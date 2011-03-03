712d46b6082e95c1b2fda27ea31bd8fcfd171bdf
Bobby Holley <bobbyholley@gmail.com>
2011-03-03 11:43:22 -0800

    fixed an indenting problem in UserInput

--- UserInput.cpp
+++ UserInput.cpp
@@ -17,67 +17,67 @@ UserInput::LoadInput(RenderContext& context)
          * and aren't communicated over the network.
          */
         switch (evt.Type) {
-        case sf::Event::Closed:
-            context.GetWindow()->Close();
-            break;
-        case sf::Event::Resized:
-            context.SetViewportAndProjection();
-            break;
-        case sf::Event::KeyPressed:
-            switch(evt.Key.Code) {
-                case sf::Key::W:
-                    context.MoveCamera(1.0, 0.0);
-                    break;
-                case sf::Key::S:
-                    context.MoveCamera(-1.0, 0.0);
-                    break;
-                case sf::Key::A:
-                    context.MoveCamera(0.0, -1.0);
-                    break;
-                case sf::Key::D:
-                    context.MoveCamera(0.0, 1.0);
-                    break;
-                case sf::Key::Left:
-                    context.MoveLight(0.0, -0.1);
-                    break;
-                case sf::Key::Right:
-                    context.MoveLight(0.0, 0.1);
-                    break;
-                case sf::Key::Down:
-                    context.MoveLight(-0.1, 0.0);
-                    break;
-                case sf::Key::Up:
-                    context.MoveLight(0.1, 0.0);
-                    break;
+            case sf::Event::Closed:
+                context.GetWindow()->Close();
+                break;
+            case sf::Event::Resized:
+                context.SetViewportAndProjection();
+                break;
+            case sf::Event::KeyPressed:
+                switch(evt.Key.Code) {
+                    case sf::Key::W:
+                        context.MoveCamera(1.0, 0.0);
+                        break;
+                    case sf::Key::S:
+                        context.MoveCamera(-1.0, 0.0);
+                        break;
+                    case sf::Key::A:
+                        context.MoveCamera(0.0, -1.0);
+                        break;
+                    case sf::Key::D:
+                        context.MoveCamera(0.0, 1.0);
+                        break;
+                    case sf::Key::Left:
+                        context.MoveLight(0.0, -0.1);
+                        break;
+                    case sf::Key::Right:
+                        context.MoveLight(0.0, 0.1);
+                        break;
+                    case sf::Key::Down:
+                        context.MoveLight(-0.1, 0.0);
+                        break;
+                    case sf::Key::Up:
+                        context.MoveLight(0.1, 0.0);
+                        break;
 
-                default:
-                    break;
-            }
-            break;
-        case sf::Event::MouseMoved:
+                    default:
+                        break;
+                }
+                break;
+            case sf::Event::MouseMoved:
 
-            // If we're not initialized, calibrate
-            if (!sMouseInitialized) {
+                // If we're not initialized, calibrate
+                if (!sMouseInitialized) {
+                    sLastMouseX = evt.MouseMove.X;
+                    sLastMouseY = evt.MouseMove.Y;
+                    sMouseInitialized = true;
+                }
+
+                // Pan the camera
+                // The arguments are +pitch and +yaw. Positive pitch looks up,
+                // positive yaw looks right.
+                // Y is relative to the top of the window, X is relative to the
+                // left of the window.
+                context.PanCamera(-(evt.MouseMove.Y - sLastMouseY),
+                                  evt.MouseMove.X - sLastMouseX);
+
+                // Save the new current value for next time
                 sLastMouseX = evt.MouseMove.X;
                 sLastMouseY = evt.MouseMove.Y;
-                sMouseInitialized = true;
-            }
+                break;
 
-            // Pan the camera
-            // The arguments are +pitch and +yaw. Positive pitch looks up,
-            // positive yaw looks right.
-            // Y is relative to the top of the window, X is relative to the
-            // left of the window.
-            context.PanCamera(-(evt.MouseMove.Y - sLastMouseY),
-                              evt.MouseMove.X - sLastMouseX);
-
-            // Save the new current value for next time
-            sLastMouseX = evt.MouseMove.X;
-            sLastMouseY = evt.MouseMove.Y;
-            break;
-
-        default:
-            break;
+            default:
+                break;
         }
 
         /*
@@ -85,7 +85,6 @@ UserInput::LoadInput(RenderContext& context)
          * we record them, and then apply them later.
          */
         switch (evt.Type) {
-
             default:
                 break;
         }
