4748c40bc5ef8d7e88095871a10d21c0e5134526
Bobby Holley <bobbyholley@gmail.com>
2011-03-10 18:59:21 -0800

    Added a Gameclock class.

---
+++ Gameclock.cpp
@@ -0,0 +1,37 @@
+#include "Gameclock.h"
+#include <assert.h>
+
+Gameclock::Gameclock(unsigned tickMS) : mTimestamp(0)
+                                      , mTickDuration(tickMS / 1000.0)
+                                      , mClockRemainder(0.0f)
+{
+}
+
+void
+Gameclock::Start()
+{
+    assert(mTimestamp == 0);
+    mClock.Reset();
+}
+
+void
+Gameclock::Tick()
+{
+    // Busywait until a tick has passed
+    float elapsedTime;
+    do {
+        elapsedTime = mClock.GetElapsedTime() + mClockRemainder;
+    } while (elapsedTime < mTickDuration);
+
+    // Determine how many ticks passed
+    unsigned nTicks = (unsigned) elapsedTime / mTickDuration;
+    assert(nTicks > 0);
+
+    // Increment the timestamp
+    mTimestamp += nTicks;
+
+    // Save the remainder
+    mClockRemainder = elapsedTime - nTicks * mTickDuration;
+    assert(mClockRemainder >= 0.0f);
+    assert(mClockRemainder < mTickDuration);
+}
